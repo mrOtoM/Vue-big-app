9dc55ac179846058cf760e1f2abd619028c3462a
mrOtoM <mrotomachala@gmail.com>
2022-11-18 16:29:37 +0100

    add logic into TheHeader.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
+  <the-header></the-header>
   <router-view></router-view>
 </template>
 
+<script>
+import TheHeader from './components/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+:root {
+  --main-gradient: linear-gradient(25deg, #d64c7f, #ee4758 50%);
+  --main-color: #f9d5e5;
+  --border-color: 1px solid #f9d5e5;
+  --white: rgb(255, 255, 255);
+}
 
 * {
   margin: 0;
