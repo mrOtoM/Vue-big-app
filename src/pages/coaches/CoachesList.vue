acdeed5cddd1bca284b96c04fd5017024025382b
mrOtoM <mrotomachala@gmail.com>
2022-11-19 09:52:31 +0100

    connection data from coaches/index.js to CoachesList.vue

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,14 +5,26 @@
       <button>Refresh</button>
       <router-link to="/register">Register as coach</router-link>
     </div>
-    <ul>
-      List of Coaches
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
     </ul>
+    <h3 v-else>No any coaches found</h3>
   </section>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
 </script>
 
 <style></style>
