07953b3888cfead1b5a1790b58b7f243c0b9ac62
mrOtoM <mrotomachala@gmail.com>
2022-11-19 09:37:30 +0100

    add some data into coaches index.js

--- src/store/modules/coaches/index.js
+++ src/store/modules/coaches/index.js
@@ -4,7 +4,30 @@ import getters from './getters.js';
 
 export default {
   namespaced: true,
-  state() {},
+  state() {
+    return {
+      coaches: [
+        {
+          id: 'c1',
+          firstName: 'Oto',
+          lastName: 'Machala',
+          areas: ['frontend', 'backend', 'career'],
+          description:
+            "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
+          hourlyRate: 30,
+        },
+        {
+          id: 'c2',
+          firstName: 'Peter',
+          lastName: 'Sveter',
+          areas: ['frontend', 'career'],
+          description:
+            'I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
+          hourlyRate: 30,
+        },
+      ],
+    };
+  },
   mutations,
   getters,
   actions,
